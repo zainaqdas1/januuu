465003da2600d5228db402dafff0c08f146fcf3d
zainaqdas1 <lovershostbuddy@gmail.com>
2017-03-21 15:25:43 +0500

    Update value.php

--- value.php
+++ value.php
@@ -20,11 +20,7 @@ $gwe=getUrl('/me',$tk,array(
 'fields' => 'id,name',
 ));
 
-$site=' ♥ c h α υ δ h r ψ s 🌹 β ο τ τ ε r ♥
-
-♡[●<~|| chaudhrys-web.tk ||~>●]♡
-
-♥♡o. Cɾεαtεɾ 💢 zɑíղ ɑզժɑՏ .o♡ ♥  
+$site='❤ CHAUDHRYS BOTTER ❤ ZAIN AQDAS ❤ ARMAN007.TK ❤  
  ';
 $true=$ops.'
  '.$site;
